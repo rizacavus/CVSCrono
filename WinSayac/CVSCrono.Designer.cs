911991666fa9a9a230a79cca13ceed0ffdbba018
Rıza Çavuşoğlu <rizacavusoglu@hotmail.com>
2023-04-04 09:59:11 +0300

    Add project files.

---
+++ WinSayac/CVSCrono.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace WinSayac
+{
+    partial class CVSCrono
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CVSCrono));
+            this.lblD = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblH = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblM = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblS = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // lblD
+            // 
+            this.lblD.AutoSize = true;
+            this.lblD.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblD.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblD.Location = new System.Drawing.Point(12, 35);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(92, 72);
+            this.lblD.TabIndex = 3;
+            this.lblD.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(92, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 72);
+            this.label4.TabIndex = 4;
+            this.label4.Text = ":";
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblH.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblH.Location = new System.Drawing.Point(122, 35);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(92, 72);
+            this.lblH.TabIndex = 5;
+            this.lblH.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(202, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 72);
+            this.label2.TabIndex = 6;
+            this.label2.Text = ":";
+            // 
+            // lblM
+            // 
+            this.lblM.AutoSize = true;
+            this.lblM.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblM.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblM.Location = new System.Drawing.Point(229, 35);
+            this.lblM.Name = "lblM";
+            this.lblM.Size = new System.Drawing.Size(92, 72);
+            this.lblM.TabIndex = 7;
+            this.lblM.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(309, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 72);
+            this.label6.TabIndex = 8;
+            this.label6.Text = ":";
+            // 
+            // lblS
+            // 
+            this.lblS.AutoSize = true;
+            this.lblS.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblS.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblS.Location = new System.Drawing.Point(338, 35);
+            this.lblS.Name = "lblS";
+            this.lblS.Size = new System.Drawing.Size(92, 72);
+            this.lblS.TabIndex = 9;
+            this.lblS.Text = "00";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnStart.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.btnStart.FlatAppearance.BorderSize = 2;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnStart.Location = new System.Drawing.Point(12, 108);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(92, 31);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnStop.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.btnStop.FlatAppearance.BorderSize = 2;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnStop.Location = new System.Drawing.Point(229, 108);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(92, 31);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnResume.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.btnResume.FlatAppearance.BorderSize = 2;
+            this.btnResume.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResume.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnResume.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnResume.Location = new System.Drawing.Point(122, 108);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(92, 31);
+            this.btnResume.TabIndex = 12;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = false;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnReset.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnReset.FlatAppearance.BorderSize = 2;
+            this.btnReset.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnReset.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnReset.Location = new System.Drawing.Point(338, 108);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(92, 31);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnClose.Location = new System.Drawing.Point(412, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(34, 32);
+            this.btnClose.TabIndex = 14;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(372, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(34, 32);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "_";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // tmr
+            // 
+            this.tmr.Interval = 1000;
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // CVSCrono
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(449, 156);
+            this.ControlBox = false;
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblS);
+            this.Controls.Add(this.lblM);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblH);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblD);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CVSCrono";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "CVSSayaç";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblD;
+        private Label label4;
+        private Label lblH;
+        private Label label2;
+        private Label lblM;
+        private Label label6;
+        private Label lblS;
+        private Button btnStart;
+        private Button btnStop;
+        private Button btnResume;
+        private Button btnReset;
+        private Button btnClose;
+        private Button button5;
+        private System.Windows.Forms.Timer tmr;
+        private NotifyIcon notifyIcon1;
+    }
+}
---
+++ WinSayac/CVSCrono.cs
@@ -0,0 +1,85 @@
+namespace WinSayac
+{
+    public partial class CVSCrono : Form
+    {
+        private TimeSpan usedTime;
+
+        public DateTime StartTime { get; set; }
+
+        public TimeSpan UsedTime
+        {
+            get => usedTime;
+            set
+            {
+                usedTime = value;
+                lblD.Text = usedTime.Days.ToString("00");
+                lblH.Text = usedTime.Hours.ToString("00");
+                lblM.Text = usedTime.Minutes.ToString("00");
+                lblS.Text = usedTime.Seconds.ToString("00");
+                Properties.Settings.Default.LastUsedTime = usedTime;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        public DateTime EndTime { get; set; }
+
+        public CVSCrono()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartTime = DateTime.Now;
+            tmr.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.LastUsedTime != (TimeSpan.Parse("00:00:00")))
+            {
+                UsedTime = Properties.Settings.Default.LastUsedTime;
+            }
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width,
+                                      workingArea.Bottom - Size.Height);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tmr.Stop();
+            UsedTime = TimeSpan.Parse("00:00:00");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            UsedTime = UsedTime.Add(TimeSpan.FromMilliseconds(tmr.Interval));
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            tmr.Stop();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tmr.Stop();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
